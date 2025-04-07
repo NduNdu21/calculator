0a46d680e3163d9aff1419f07ad944cfe1d6f06c
NduNdu21 <ndumisobm@icloud.com>
2025-04-07 15:33:56 +0100

    Query selectors for buttons and display

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+//Query selectors
+const c1 = document.querySelector("#1")
+const c2 = document.querySelector("#2")
+const c3 = document.querySelector("#3")
+const c4 = document.querySelector("#4")
+const c5 = document.querySelector("#5")
+const c6 = document.querySelector("#6")
+const c7 = document.querySelector("#7")
+const c8 = document.querySelector("#8")
+const c9 = document.querySelector("#9")
+const c0 = document.querySelector("#0")
+
+const add = document.querySelector("#add")
+const sub = document.querySelector("#sub")
+const div = document.querySelector("#div")
+const mult = document.querySelector("#mult")
+const ac = document.querySelector("#AC")
+const equal = document.querySelector("#equal")
+const del = document.querySelector("#DEL")
+
+const display = document.querySelector(".display")
+
+
+//Event listeners
